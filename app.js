de22f88ba5d1d22ab3b190ab8fe22d769b9a3c0d
Yonny Arias <yonny921@gmail.com>
2023-08-14 15:58:26 -0500

    mod app port

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const multer = require('multer');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Configuración de Multer para la carga de imágenes
 const storage = multer.diskStorage({
